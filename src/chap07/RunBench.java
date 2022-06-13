c79c1c8cde7943f6d8c5837b3e5d2d5d057b5cdd
”Вагин <m.vagin@k-telecom.org>
2022-06-13 21:27:11 +0500

    add chap07

---
+++ src/chap07/RunBench.java
@@ -0,0 +1,11 @@
+package chap07;
+
+import org.openjdk.jmh.runner.RunnerException;
+
+import java.io.IOException;
+
+public class RunBench {
+    public static void main(String[] args) throws IOException, RunnerException {
+        org.openjdk.jmh.Main.main(args);
+    }
+}
