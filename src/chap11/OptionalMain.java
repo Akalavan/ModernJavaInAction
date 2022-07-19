fd9a57558fac8fa0b50091937163e199b4e20a84
”Вагин <m.vagin@k-telecom.org>
2022-07-19 23:46:15 +0500

    add chap11

--- src/chap11/OptionalMain.java
+++ src/chap11/OptionalMain.java
@@ -1,6 +1,12 @@
 package chap11;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author Вагин Михаил
@@ -53,6 +59,43 @@ public class OptionalMain {
   }*/
     public String getCarInsuranceName(Optional<Person> person) {
         return person
+                .flatMap(Person::getCar)
+                .flatMap(Car::getInsurance)
+                .map(Insurance::getName)
+                .orElse("Unknown");
+    }
+
+    public Set<String> getCarInsuranceNames(List<Person> people) {
+        return people.stream()
+                .map(Person::getCar)
+                .map(optCar -> optCar.flatMap(Car::getInsurance))
+                .map(optIns -> optIns.map(Insurance::getName))
+                .flatMap(Optional::stream)
+                .collect(Collectors.toSet());
+    }
+
+    public Insurance findCheapestInsurance(Person person, Car car) {
+        // queries services provided by the different insurance companies
+        // compare all those data
+        Insurance cheapestCompany = new Insurance();
+        return cheapestCompany;
+    }
+
+    public Optional<Insurance> nullSafeFindCheapestInsurance(Optional<Person> person, Optional<Car> car) {
+        if (person.isPresent() && car.isPresent()) {
+            return Optional.of(findCheapestInsurance(person.get(), car.get()));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<Insurance> nullSafeFindCheapestInsuranceQuiz(Optional<Person> person, Optional<Car> car) {
+        return person.flatMap(p -> car.map(c -> findCheapestInsurance(p, c)));
+    }
+
+    public String getCarInsuranceName(Optional<Person> person, int minAge) {
+        return person
+                .filter(p -> p.getAge() >= minAge)
                 .flatMap(Person::getCar)
                 .flatMap(Car::getInsurance)
                 .map(Insurance::getName)
